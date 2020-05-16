893a24b1823f5171545cac1f8fb8a301223f94e4
David Rosales Robles <drosar00@estudiantes.unileon.es>
2020-05-16 20:51:27 +0200

    restaurar clase borrada

---
+++ src/main/java/controlador/CreateRecipeControler.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Javier
+ */
+
+@Named
+@ViewScoped
+public class CreateRecipeControler implements Serializable{
+    
+    
+    @PostConstruct
+    public void inicio() {
+
+    }
+
+    
+}
